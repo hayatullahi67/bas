326853739fcb75d8d9b0e9491731d48bd4b023b2
hayatullahi67 <wahadisllahi@gmail.com>
2026-01-01 00:23:22 +0100

    height

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -4,7 +4,7 @@ import { ArrowRight } from 'lucide-react';
 
 const Hero = () => {
   return (
-    <section className="relative h-[100vh] md:min-h-[90vh] flex items-center overflow-hidden">
+    <section className="relative h-[100px] md:min-h-[90vh] flex items-center overflow-hidden">
         {/* Background Image with Overlay */}
         <div
           className="absolute inset-0 z-0"
